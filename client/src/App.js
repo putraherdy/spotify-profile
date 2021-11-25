de0a6837ea05356a7c96ec3831d396c4b8ebe9fc
Herdy Dwi Putra <herdydwiputra@gmail.com>
2021-11-25 20:43:44 +0700

    Deployment

---
+++ client/src/App.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+import { accessToken, logout } from './spotify';
+import {
+  Login,
+  Profile,
+  TopArtists,
+  TopTracks,
+  Playlists,
+  Playlist,
+} from './pages';
+import { GlobalStyle } from './styles';
+import styled from 'styled-components/macro';
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0, 0, 0, 0.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
+
+// Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    setToken(accessToken);
+  }, []);
+
+  return (
+    <div className="app">
+      <GlobalStyle />
+
+      {!token ? (
+        <Login />
+      ) : (
+        <>
+          <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+
+          <Router>
+            <ScrollToTop />
+
+            <Switch>
+              <Route path="/top-artists">
+                <TopArtists />
+              </Route>
+              <Route path="/top-tracks">
+                <TopTracks />
+              </Route>
+              <Route path="/playlists/:id">
+                <Playlist />
+              </Route>
+              <Route path="/playlists">
+                <Playlists />
+              </Route>
+              <Route path="/">
+                <Profile />
+              </Route>
+            </Switch>
+          </Router>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/styles/fonts.js
@@ -0,0 +1,26 @@
+import { css } from 'styled-components/macro';
+
+const fonts = css`
+  @font-face {
+    font-family: 'Circular Std';
+    src: url('../fonts/CircularStd-Book.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Circular Std';
+    src: url('../fonts/CircularStd-Bold.woff') format('woff');
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Circular Std';
+    src: url('../fonts/CircularStd-Black.woff') format('woff');
+    font-weight: 900;
+    font-style: normal;
+  }
+`;
+
+export default fonts;
